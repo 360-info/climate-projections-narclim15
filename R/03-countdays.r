4c04f2cd6b37843346789f9f5460af8c398aa196
James Goldie <james.goldie@360info.org>
2022-10-28 15:55:32 +1100

    Assert periods

--- R/03-countdays.r
+++ R/03-countdays.r
@@ -1,13 +1,14 @@
 # create the destination folder if it's missing
 counted_folder <- create_storedir_if_missing("03-counted")
 
-# count_days_gte: given a netcdf path, a threshold (in °C) and a variable name
+# count_annual_days_gte: given a netcdf path, a threshold (in °C) and a variable name
 # in the netcdf, return the path to a netcdf with the annual number of days
 # greater than or equal to the threshold
 count_annual_days_gte <- function(path, thresh = 35) {
 
   # assemble output file name and path
   out_file <- paste0(
+    "gte",
     str_replace(thresh, fixed("."), "p"), "_",
     basename(path))
 
@@ -28,12 +29,24 @@ count_annual_days_gte <- function(path, thresh = 35) {
 }
 
 # extract_path_metadata: extract the encoded netcdf metadata in narclim
-# source filenames as a tibble
-extract_counted_metadata <- function(paths) {
+# source filenames as a tibble. group start and end dates into year periods
+extract_counted_metadata <- function(paths, periods) {
+
   tibble(path = paths, fname = basename(path)) |>
     separate(fname,
       into = c("thresh", "var", "grid", "gcm", "scenario", "run",
         "rcm", "version", "time", "years", "ext"),
       sep = "[_.]") |>
-    dplyr::select(-version, -time)
+    dplyr::select(-version, -time, -ext) |>
+    separate(years, into = c("yr_start", "yr_end"), sep = "-") |>
+    mutate(
+      across(starts_with("yr_"), ymd),
+      yr_start_bin = cut(yr_start, breaks = periods,
+        labels = head(names(periods), -1)),
+      yr_end_bin = cut(yr_end, breaks = periods,
+        labels = head(names(periods), -1)))
+}
+
+count_year_overlaps <- function(df) {
+  df |> filter(yr_start_bin != yr_end_bin) |> nrow()
 }
--- _targets.r
+++ _targets.r
@@ -1,4 +1,5 @@
 library(targets)
+library(tarchetypes)
 
 source("R/00-util.r")
 source("R/01-download.r")
@@ -6,7 +7,7 @@ source("R/02-unzip.r")
 source("R/03-countdays.r")
 
 tar_option_set(packages = c(
-  "dplyr", "stringr", "tibble", "tidyr", "ClimateOperators"))
+  "dplyr", "lubridate", "stringr", "tibble", "tidyr", "ClimateOperators"))
 
 # add collection ids from climatedata-beta.environment.nsw.gov.au here to
 # process other data (eg. minimum temperatures)
@@ -18,6 +19,18 @@ collections <- c(
 # temperature thresholds
 selected_thresholds <- c(35, 37.5)
 
+# note: cut.Date() by default does intervals of [left, right)
+year_breaks <- as.Date(c(
+  # historical
+  `1995` = "1986-01-01",
+  `2015` = "2006-01-01",
+  # rcps
+  `2030` = "2021-01-01",
+  `2050` = "2041-01-01",
+  `2070` = "2061-01-01",
+  `2090` = "2081-01-01",
+  `2110` = "2101-01-01"))
+
 # here's the pipeline to run:
 list(
 
@@ -25,6 +38,7 @@ list(
   tar_target(collection_ids, collections),
   tar_target(collection_names, names(collections)),
   tar_target(thresholds, selected_thresholds),
+  tar_target(periods, year_breaks),
 
   # 1) download the zip files
   tar_target(dl_data,
@@ -40,12 +54,20 @@ list(
     format = "file"),
   tar_target(unzip_data_all, unzip_data),
 
-  # 3) calculate days >= 35 or 37.5 C
+  # 3) calculate days >= 35 or 37.5 C (group files for year_block stats)
   tar_target(count_days,
     count_annual_days_gte(unzip_data_all, thresholds),
     pattern = cross(unzip_data_all, thresholds),
     format = "file"),
-  tar_target(counted_metadata, extract_counted_metadata(count_days))
+  tar_group_by(counted_metadata,
+    extract_counted_metadata(count_days, periods),
+    thresh, var, grid, gcm, scenario, run, rcm, year_start_bin),
+  tar_target(year_overlap_ok, tar_assert_identical(
+    count_year_overlaps(counted_metadata), 0L,
+    "Some of the year_breaks overlap file boundaries."))
 
-  # 4) group by model + scenario + years;
+  # 4) count year block stats (typical/hot/cold)
+  tar_target(
+
+  )
 )
