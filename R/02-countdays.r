3a14c99c54a706b43a24f6835423472465a55ac0
James Goldie <james.goldie@360info.org>
2022-11-15 11:05:33 +1100

    Remove poor AWAP quality mask regions

--- R/02-countdays.r
+++ R/02-countdays.r
@@ -1,10 +1,11 @@
 # create the destination folder if it's missing
 counted_folder <- create_storedir_if_missing("02-counted")
+scratch_folder <- create_storedir_if_missing("scratch")
 
 # count_annual_days_gte: given a netcdf path, a threshold (in °C) and a variable name
 # in the netcdf, return the path to a netcdf with the annual number of days
 # greater than or equal to the threshold
-count_annual_days_gte <- function(path, thresh = 35, apply_mask = FALSE) {
+count_annual_days_gte <- function(path, thresh = 35) {
 
   # assemble output file name and path
   out_file <- paste0(
@@ -13,24 +14,52 @@ count_annual_days_gte <- function(path, thresh = 35, apply_mask = FALSE) {
     basename(path))
   out_path <- file.path(counted_folder, out_file)
 
-  # run cdo:
-  # - mark days as 1 if >= threshold (convert °C to K)
-  # - count all such days each year
+  # if there's an awap quality mask, we need to remove regions not
+  # covered by the mask first
+  if (cdo("showname", path) |> str_detect(fixed("AWAP_qualitymask"))) {
 
-  # TODO - mask out AWAP quality mask first, if AWAP_qualitymask var exists
-
-  if (apply_mask) {
-
-  } else {
+    # a) extract mask to separate file (to scratch)
+    mask_path <- tempfile("mask_", here("_targets", "scratch"), ".nc")
     cdo(
       "-L",
-      "yearsum",
-      csl("-gec", thresh + 273.15),
+      csl("setctomiss", "0"),
+      csl("-selname", "AWAP_qualitymask"),
       path,
-      out_path)
+      mask_path)
+    
+    # b) remove unmasked regions (to scratch)
+    # cdo div file1.nc mask.nc out.nc
+    obs_path <- tempfile("out_", here("_targets", "scratch"), ".nc")
+    cdo(
+      "-L",
+      "div",
+      path,
+      mask_path,
+      obs_path)
+
+    # cleanup temp mask file
+    unlink(mask_path)
+
+  } else {
+    # no awap quality mask: just count from the downloaded file directly
+    obs_path <- path
   }
 
+  # c) count days from masked regions
+  # - mark days as 1 if >= threshold (convert °C to K)
+  # - count all such days each year
+  cdo(
+    "-L",
+    "yearsum",
+    csl("-gec", thresh + 273.15),
+    csl("-selname", "tasmax-bc"),
+    obs_path,
+    out_path)
+
+  # cleanup temp file before we return
+  if (obs_path != path) {
+    unlink(obs_path)
+  }
 
   return(out_path)
 }
-
