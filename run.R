fb30391d49279ce226109ddd118ca65283401e12
James Goldie <james.goldie@360info.org>
2022-11-15 12:29:45 +1100

    Add optional tar_watch to run.R

--- run.R
+++ run.R
@@ -5,6 +5,10 @@
 # See https://books.ropensci.org/targets/hpc.html
 # to learn about your options.
 
+# uncomment if you'd like to watch the build process with shiny as it runs
+# targets::tar_watch(seconds = 10, outdated = FALSE, targets_only = TRUE,
+#   label = "branches")
+
 targets::tar_make()
 # targets::tar_make_clustermq(workers = 2) # nolint
 # targets::tar_make_future(workers = 2) # nolint
